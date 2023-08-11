16ad688d48eb21fbae9123fbed8ca28d2d7258d3
MagnusHLund <magnusherringelund@gmail.dk>
2023-08-11 12:10:21 +0200

    Updated code, as well as comments

--- H1_Dice/Program.cs
+++ H1_Dice/Program.cs
@@ -4,34 +4,20 @@
     {
         static void Main()
         {
+            // Creates a Random variable and assigns the name "random"
             Random random = new Random();
 
+            // Creates an infinite loop
             while (true)
             {
+                // Creates a new variable called "dice" and assigns a random value, which is between 1 and 6
                 int dice = random.Next(1, 7);
+
+                // Changes the text color to any of the colors available, as well writing "Du slog {dice}" in the console.
                 Console.ForegroundColor = (ConsoleColor)random.Next(0, 16);
-                Console.Write("Du slog ");
-                switch (dice)
-                {
-                    case 1:
-                        Console.Write(dice);
-                        break;
-                    case 2:
-                        Console.Write(dice);
-                        break;
-                    case 3:
-                        Console.Write(dice);
-                        break;
-                    case 4:
-                        Console.Write(dice);
-                        break;
-                    case 5:
-                        Console.Write(dice);
-                        break;
-                    default:
-                        Console.Write(dice);
-                        break;
-                }
+                Console.Write($"Du slog {dice}");
+
+                // Uses a ReadLine, to avoid spam in the console, as well as clearing the console.
                 Console.ReadLine();
                 Console.Clear();
             }
